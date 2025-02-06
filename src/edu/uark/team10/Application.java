71289cfb75dc988b3266edd71a4c7998e4bd3e31
WestinPickell1 <westinpickell@yahoo.com>
2025-02-06 14:18:40 -0600

    Update Application.java

--- src/edu/uark/team10/Application.java
+++ src/edu/uark/team10/Application.java
@@ -1,17 +1,16 @@
 package edu.uark.team10;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseListener;
-import java.io.File;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Application extends JFrame { // JFrame lets us create windows
     
